46f7471a3e6c6e3649f9d4cde3ebdcc79c72e821
cordialDisarray <166568225+cordialDisarray@users.noreply.github.com>
2025-06-02 08:20:55 +0200

    Generated the project

---
+++ src/main/java/rs/ac/singidunum/basic_ticket_manager/BasicTicketManagerApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.singidunum.basic_ticket_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicTicketManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicTicketManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rs/ac/singidunum/basic_ticket_manager/BasicTicketManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.singidunum.basic_ticket_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicTicketManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
